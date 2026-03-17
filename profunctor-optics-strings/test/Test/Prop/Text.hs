f4a8d1f665b8aabd9b36b66ff794c1fda3cd802f
cmk <6869885+cmk@users.noreply.github.com>
2026-03-17 02:48:00 -0700

    test: Expand string-optics API & prop tests
    Add text-short: shortText iso for Data.Text.Optic
    
    Test.Prop.Word (11):
      bits8/16/32/64 identity, bits8 not = complement, bits8 not.not = id,
      toBits/fromBits round-trips for all sizes, grate8 identity
    
    Test.Prop.ByteString (5):
      short round-trip (both directions), lazy round-trip,
      packed round-trip (both directions)
    
    Test.Prop.Text (4):
      lazy round-trip, packed round-trip (both directions), utf8 round-trip

---
+++ profunctor-optics-strings/test/Test/Prop/Text.hs
@@ -0,0 +1,71 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Test.Prop.Text (tests) where
+
+import Hedgehog
+import qualified Hedgehog.Gen as Gen
+import qualified Hedgehog.Range as Range
+
+import qualified Data.Text as T
+import qualified Data.Text.Lazy as TL
+import qualified Data.Text.Encoding as TE
+import qualified Data.Text.Short as ST
+import Data.Text.Optic
+import Data.Profunctor.Optic
+
+tests :: IO Bool
+tests = checkParallel $$(discover)
+
+---------------------------------------------------------------------
+-- short
+---------------------------------------------------------------------
+
+-- | view short . review short = id
+prop_short_roundtrip :: Property
+prop_short_roundtrip = property $ do
+    s <- forAll $ Gen.text (Range.linear 0 100) Gen.unicode
+    review short (view short s) === s
+
+-- | review short . view short = id
+prop_short_roundtrip_rev :: Property
+prop_short_roundtrip_rev = property $ do
+    s <- forAll $ Gen.text (Range.linear 0 100) Gen.unicode
+    let st = ST.fromText s
+    view short (review short st) === st
+
+---------------------------------------------------------------------
+-- lazy
+---------------------------------------------------------------------
+
+-- | view lazy . review lazy = id
+prop_lazy_roundtrip :: Property
+prop_lazy_roundtrip = property $ do
+    s <- forAll $ Gen.text (Range.linear 0 100) Gen.unicode
+    review lazy (view lazy s) === s
+
+---------------------------------------------------------------------
+-- packed
+---------------------------------------------------------------------
+
+-- | view packed . review packed = id
+prop_packed_roundtrip :: Property
+prop_packed_roundtrip = property $ do
+    s <- forAll $ Gen.string (Range.linear 0 100) Gen.unicode
+    review packed (view packed s) === s
+
+-- | review packed . view packed = id
+prop_packed_roundtrip_rev :: Property
+prop_packed_roundtrip_rev = property $ do
+    s <- forAll $ Gen.text (Range.linear 0 100) Gen.unicode
+    view packed (review packed s) === s
+
+---------------------------------------------------------------------
+-- utf8
+---------------------------------------------------------------------
+
+-- | utf8 round-trips for valid text
+prop_utf8_roundtrip :: Property
+prop_utf8_roundtrip = property $ do
+    s <- forAll $ Gen.text (Range.linear 0 100) Gen.unicode
+    review utf8 (view utf8 s) === s
