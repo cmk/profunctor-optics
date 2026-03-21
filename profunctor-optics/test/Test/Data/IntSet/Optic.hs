ea4a3a4df97cc4c8643b95d328450dbecdacf53d
cmk <6869885+cmk@users.noreply.github.com>
2026-03-21 02:00:12 -0700

    Sprint 15: Add Set, IntMap, IntSet optics + lazy Map variants
    Data.Set.Optic: member (Fold0), folded (Fold), listed (Iso)
    Data.IntMap.Optic: at, iat, values, imapped, ifiltered, itraversed,
      ifolded, altered/altered' (strict/lazy), ialtered, alteredF,
      ialteredF, adjusted, updated, lookedMin/Max, validated,
      toIntMapOfL, countingIntMapOfL
    Data.IntSet.Optic: member (Fold0), folded (Fold), listed (Iso)
    Data.Map.Optic: altered/ialtered now strict (MapS.alter),
      added altered'/ialtered' as lazy variants
    
    Tests: 3 Set + 4 IntMap + 3 IntSet = 10 new properties.
    89 total, all green.

---
+++ profunctor-optics/test/Test/Data/IntSet/Optic.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module Test.Data.IntSet.Optic (tests) where
+
+import Hedgehog
+import qualified Hedgehog.Gen as Gen
+import qualified Hedgehog.Range as Range
+
+import Data.IntSet.Optic as ISO
+import Data.Profunctor.Optic hiding (folded)
+import qualified Data.IntSet as IS
+
+tests :: IO Bool
+tests = checkParallel $$(discover)
+
+genIntSet :: Gen IS.IntSet
+genIntSet = IS.fromList <$> Gen.list (Range.linear 0 20) (Gen.int (Range.linear 0 50))
+
+-- folded collects all elements
+prop_folded_count :: Property
+prop_folded_count = property $ do
+    s <- forAll genIntSet
+    length (s ^.. folded) === IS.size s
+
+-- folded elements are ascending
+prop_folded_ascending :: Property
+prop_folded_ascending = property $ do
+    s <- forAll genIntSet
+    let xs = s ^.. folded
+    xs === IS.toAscList s
+
+-- listed roundtrip
+prop_listed_roundtrip :: Property
+prop_listed_roundtrip = property $ do
+    s <- forAll genIntSet
+    review listed (view listed s) === s
